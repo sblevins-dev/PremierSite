fa3b5890fff4a91ae13766f907564a8685125067
Stephen Blevins <scblevins27@gmail.com>
2022-06-14 17:34:46 -0400

    added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './css/nav.css';
 import { Nav } from './components/Nav'
 import { Home } from './components/Home'
+import { Footer } from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Home />
+      <Footer />
     </div>
   );
 }
