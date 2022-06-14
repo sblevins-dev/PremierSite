671b6b18923bc0ffa4bb73d0626b52205dc7697c
Stephen Blevins <scblevins27@gmail.com>
2022-06-14 16:12:18 -0400

    added nav

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,7 +1,21 @@
-import '../css/nav.css'
+import "../css/nav.css";
 
 export const Nav = () => {
   return (
-    <div className="nav-container">Nav</div>
-  )
-}
+    <div className="nav-container">
+      <div className="hamburger">Ham</div>
+
+      {/* Right Side of Nav */}
+      <div className="title">Title</div>
+      <div className="option-wrapper">
+        <div className="acct-nav-btns">
+          <div className="login-btn">Log In</div>
+          <div>|</div>
+          <div className="create-acct-btn">Create Account</div>
+        </div>
+        <div className="search-box">Search</div>
+        <div className="cart-on-nav">Cart</div>
+      </div>
+    </div>
+  );
+};
