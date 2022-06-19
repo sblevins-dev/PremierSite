ba928c23190dc527ebceecbd402b1228d17f8ac3
Stephen Blevins <scblevins27@gmail.com>
2022-06-19 17:20:56 -0400

    added functionality between login and register modals

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,36 +1,50 @@
-import '../css/login.css'
-import { useContext, useEffect } from 'react'
-import { Context } from '../contexts/Context'
+import "../css/login.css";
+import { useContext, useEffect } from "react";
+import { Context } from "../contexts/Context";
 
 export const Login = () => {
-    const {loginRef, isLoginShown, setLoginShown} = useContext(Context)
+  const { loginRef, isLoginShown, setLoginShown, setRegisterShown } = useContext(Context);
 
-    const handle = (e) => {
-            if (!loginRef.current?.contains(e.target) && isLoginShown) {
-                setLoginShown(false)
-                document.querySelector('.login-container').removeEventListener('click', handle)
-            }
+  // add event listener
+  useEffect(() => {
+    document
+      .querySelector(".login-container")
+      .addEventListener("click", handle);
+  });
+
+  // handle outside click
+  const handle = (e) => {
+    if (!loginRef.current?.contains(e.target) && isLoginShown) {
+      setLoginShown(false);
+      document
+        .querySelector(".login-container")
+        .removeEventListener("click", handle);
     }
+  };
 
-    useEffect(() => {
-        document.querySelector('.login-container').addEventListener('click', handle)
-    })
+  // handle register link
+  const handleRegisterClick = () => {
+    setLoginShown(false)
+    setRegisterShown(true)
+  }
 
   return (
     <div className="login-container">
-        <div className="login-modal-wrapper" ref={loginRef}>
-            <div className='login-img'>hello</div>
-            <form className='login-form'>
-                <h1>Login</h1>
-                <p>Don't have an account? Sign up here</p>
-                <div className='form-group'>
-                    <input type='email' placeholder='Email' />
-                </div>
-                <div className='form-group'>
-                    <input type='password' placeholder='Password' />
-                </div>
-            </form>
-        </div>
+      <div className="login-modal-wrapper" ref={loginRef}>
+        <div className="login-img">hello</div>
+        <form className="login-form">
+          <h1>Login</h1>
+          <p>
+            Don't have an account? <p className="register-link" onClick={handleRegisterClick}>Sign up here</p>
+          </p>
+          <div className="form-group">
+            <input type="email" placeholder="Email" />
+          </div>
+          <div className="form-group">
+            <input type="password" placeholder="Password" />
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -3,9 +3,17 @@ import { useContext, useEffect } from "react";
 import { Context } from "../contexts/Context";
 
 export const Register = () => {
-  const { registerRef, isRegisterShown, setRegisterShown } =
+  const { registerRef, isRegisterShown, setRegisterShown, setLoginShown } =
     useContext(Context);
 
+    // add event listener
+  useEffect(() => {
+    document
+      .querySelector(".register-container")
+      .addEventListener("click", handle);
+  });
+
+  // handle outside click
   const handle = (e) => {
     if (!registerRef.current?.contains(e.target) && isRegisterShown) {
       setRegisterShown(false);
@@ -15,11 +23,11 @@ export const Register = () => {
     }
   };
 
-  useEffect(() => {
-    document
-      .querySelector(".register-container")
-      .addEventListener("click", handle);
-  });
+  // handle login link
+  const handleLoginClick = () => {
+    setRegisterShown(false)
+    setLoginShown(true)
+  }
 
   return (
     <div className="register-container">
@@ -27,7 +35,12 @@ export const Register = () => {
         <div className="register-img">hello</div>
         <form className="register-form">
           <h1>Register</h1>
-          <p>Already have an account? Sign in here</p>
+          <p>
+            Already have an account?{" "}
+            <p className="log-in-link" onClick={handleLoginClick}>
+              Sign in here
+            </p>
+          </p>
           <div className="form-group">
             <input type="email" placeholder="Email" />
           </div>
