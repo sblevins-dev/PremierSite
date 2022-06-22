bd61ffccbc82fe814aba14920522f7325e858cc6
Stephen Blevins <scblevins27@gmail.com>
2022-06-22 14:58:16 -0400

    added react-transition-group to login and register modals

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,15 +1,21 @@
 import "../css/login.css";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Context } from "../contexts/Context";
+import { Transition } from "react-transition-group";
 
 export const Login = () => {
-  const { loginRef, isLoginShown, setLoginShown, setRegisterShown } = useContext(Context);
+  const { loginRef, isLoginShown, setLoginShown, setRegisterShown } =
+    useContext(Context);
+
+  const [mountProp, setMountProp] = useState(false)
 
   // add event listener
   useEffect(() => {
     document
       .querySelector(".login-container")
       .addEventListener("click", handle);
+
+    setMountProp(true)
   });
 
   // handle outside click
@@ -24,27 +30,50 @@ export const Login = () => {
 
   // handle register link
   const handleRegisterClick = () => {
-    setLoginShown(false)
-    setRegisterShown(true)
+    setLoginShown(false);
+    setRegisterShown(true);
+  };
+
+  const duration = 500
+
+  const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 0
+  }
+
+  const transitionStyles = {
+    entering: { opacity: 1 },
+    entered: {opacity: 1 },
   }
 
   return (
-    <div className="login-container">
-      <div className="login-modal-wrapper" ref={loginRef}>
-        <div className="login-img">hello</div>
-        <form className="login-form">
-          <h1>Login</h1>
-          <p>
-            Don't have an account? <p className="register-link" onClick={handleRegisterClick}>Sign up here</p>
-          </p>
-          <div className="form-group">
-            <input type="email" placeholder="Email" />
-          </div>
-          <div className="form-group">
-            <input type="password" placeholder="Password" />
-          </div>
-        </form>
+    <Transition in={mountProp}>
+      {state => (
+        <div className="login-container" style={{
+          ...defaultStyle,
+          ...transitionStyles[state]
+        }}>
+        <div className="login-modal-wrapper" ref={loginRef}>
+          <div className="login-img">hello</div>
+          <form className="login-form">
+            <h1>Login</h1>
+            <p>
+              Don't have an account?{" "}
+              <span className="register-link" onClick={handleRegisterClick}>
+                Sign up here
+              </span>
+            </p>
+            <div className="form-group">
+              <input type="email" placeholder="Email" />
+            </div>
+            <div className="form-group">
+              <input type="password" placeholder="Password" />
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+      )}
+      
+    </Transition>
   );
 };
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,16 +1,21 @@
 import "../css/register.css";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Context } from "../contexts/Context";
+import { Transition } from "react-transition-group";
 
 export const Register = () => {
   const { registerRef, isRegisterShown, setRegisterShown, setLoginShown } =
     useContext(Context);
 
-    // add event listener
+  const [mountProp, setMountProp] = useState(false);
+
+  // add event listener
   useEffect(() => {
     document
       .querySelector(".register-container")
       .addEventListener("click", handle);
+
+    setMountProp(true);
   });
 
   // handle outside click
@@ -25,30 +30,53 @@ export const Register = () => {
 
   // handle login link
   const handleLoginClick = () => {
-    setRegisterShown(false)
-    setLoginShown(true)
-  }
+    setRegisterShown(false);
+    setLoginShown(true);
+  };
+
+  // fade in
+  const duration = 500;
+
+  const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 0,
+  };
+
+  const transitionStyles = {
+    entering: { opacity: 1 },
+    entered: { opacity: 1 },
+  };
 
   return (
-    <div className="register-container">
-      <div className="register-modal-wrapper" ref={registerRef}>
-        <div className="register-img">hello</div>
-        <form className="register-form">
-          <h1>Register</h1>
-          <p>
-            Already have an account?{" "}
-            <p className="log-in-link" onClick={handleLoginClick}>
-              Sign in here
-            </p>
-          </p>
-          <div className="form-group">
-            <input type="email" placeholder="Email" />
+    <Transition in={mountProp}>
+      {(state) => (
+        <div
+          className="register-container"
+          style={{
+            ...defaultStyle,
+            ...transitionStyles[state],
+          }}
+        >
+          <div className="register-modal-wrapper" ref={registerRef}>
+            <div className="register-img">hello</div>
+            <form className="register-form">
+              <h1>Register</h1>
+              <p>
+                Already have an account?{" "}
+                <span className="log-in-link" onClick={handleLoginClick}>
+                  Sign in here
+                </span>
+              </p>
+              <div className="form-group">
+                <input type="email" placeholder="Email" />
+              </div>
+              <div className="form-group">
+                <input type="password" placeholder="Password" />
+              </div>
+            </form>
           </div>
-          <div className="form-group">
-            <input type="password" placeholder="Password" />
-          </div>
-        </form>
-      </div>
-    </div>
+        </div>
+      )}
+    </Transition>
   );
 };
