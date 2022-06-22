9cd950ab8138813f9ca50d06f3d048916808b2d9
Stephen Blevins <scblevins27@gmail.com>
2022-06-22 15:55:48 -0400

    used transition and css animation for hero and featured

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,6 +3,7 @@ import { ProductCard } from "./ProductCard";
 
 export const Home = () => {
   let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
   return (
     <div className="home-container">
       <div className="categories-wrapper">
@@ -15,6 +16,7 @@ export const Home = () => {
         <span>Option</span>
       </div>
       <div className="hero">Hero</div>
+
       <div className="featured-wrapper">
         <div className="first">first</div>
         <div className="second">first</div>
@@ -25,9 +27,7 @@ export const Home = () => {
           <ProductCard key={num} data={num} />
         ))}
       </div>
-      <div className="featured-product-wrapper">
-        
-      </div>
+      <div className="featured-product-wrapper"></div>
     </div>
   );
 };
