e64010fb9e94361d294e51f75a19b4f4e0988424
Stephen Blevins <scblevins27@gmail.com>
2022-06-27 17:19:22 -0400

    added plus icon and stock photo

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,4 +1,6 @@
 import "../css/productCard.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCirclePlus} from '@fortawesome/free-solid-svg-icons'
 
 export const ProductCard = ({ data, delay }) => {
   return (
@@ -6,7 +8,11 @@ export const ProductCard = ({ data, delay }) => {
       className="product-card-wrapper product-animation"
       style={{ animationDelay: `${delay}s` }}
     >
-      <div className="product-card">{data}</div>
+      <div className="product-card">
+        {/* {data} */}
+        <FontAwesomeIcon className="plus-icon" icon={faCirclePlus} size="xl" />
+        <img className="product-img" src="https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6441/6441177_sd.jpg;maxHeight=200;maxWidth=300"></img>
+      </div>
       <div className="product-details">
         <div className="product-name">Name</div>
         <div className="product-price">$0</div>
