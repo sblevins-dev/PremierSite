7546deaadcd4ee6ba133640bc4dd98638de4d161
Stephen Blevins <scblevins27@gmail.com>
2022-06-30 18:55:23 -0400

    edited footer only a little bit

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,6 +2,11 @@ import '../css/footer.css'
 
 export const Footer = () => {
   return (
-    <div className="footer-container">Footer</div>
+    <div className="footer-container">
+      <div>All Products</div>
+      <div>Test</div>
+      <div>Terms of Use</div>
+      <div>Privacy &amp; Security</div>
+    </div>
   )
 }
