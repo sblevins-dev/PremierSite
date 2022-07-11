7710f13962ad91e4d72265d6674bb83fd804ca71
Stephen Blevins <scblevins27@gmail.com>
2022-07-11 14:34:46 -0400

    some changes to checkout page

--- frontend/src/components/Checkout.js
+++ frontend/src/components/Checkout.js
@@ -17,6 +17,7 @@ export const Checkout = () => {
   };
 
   const [shipping, setShipping] = useState(initialShipping);
+  const { fName, lName, address, city, state, zip } = shipping;
 
   const handleShippingChange = (e) => {
     setShipping({
@@ -29,9 +30,25 @@ export const Checkout = () => {
     navigate("/");
   };
 
-  const handleShipping = () => {
-    setFlag('shipping-method')
+  const handleReturnToAddress = (e) => {
+    e.preventDefault()
+    setFlag('shipping')
+  }
+
+  const handleShipping = (e) => {
+    e.preventDefault();
+    setFlag("shipping-method");
   };
+
+  const handleShippingMethod = (e) => {
+    e.preventDefault();
+    setFlag("payment");
+  };
+
+  const handleReturnToShipping = (e) => {
+    e.preventDefault()
+    setFlag("shipping-method")
+  }
 
   return (
     <div className="checkout-page-container">
@@ -97,21 +114,108 @@ export const Checkout = () => {
               </div>
             </form>
           )}
-          {flag === 'shipping-method' && (
-            <form className="shipping-method-wrapper">
-                <div className="option">
-                    <input type="radio" />
-                    <p>Standard</p>
+          {flag !== "shipping" &&
+            flag !== "payment" &&
+            fName !== "" &&
+            lName !== "" &&
+            address !== "" &&
+            city !== "" &&
+            state !== "" &&
+            zip !== 0 && (
+              <form className="shipping-method-wrapper">
+                <h2>Shipping Method</h2>
+                <div>
+                  <input
+                    name="shipping-method"
+                    type="radio"
+                    value="standard"
+                    title="standard"
+                  />
+                  <label forhtml="standard">
+                    <h3>Ground</h3>
+                    <p>
+                      Standard shipping. Receive your items within 5-7 business
+                      days.
+                    </p>
+                  </label>
                 </div>
-                <div className="option">
-                    <input type="radio" />
-                    <p>Expedited</p>
+                <div>
+                  <input
+                    name="shipping-method"
+                    type="radio"
+                    value="next-day"
+                    title="next-day"
+                  />
+                  <label forhtml="next-day">
+                    <h3>Next Day Air</h3>
+                    <p>Get it by the next business day!</p>
+                  </label>
                 </div>
-                <div className="option">
-                    <input type="radio" />
-                    <p>Next-Day Air</p>
+                <div className="shipping-method-btns">
+                  <button
+                    className="return-to-address"
+                    onClick={handleReturnToAddress}
+                  >
+                    Return to Address
+                  </button>
+                  <button
+                    className="continue-payment"
+                    onClick={handleShippingMethod}
+                  >
+                    Continue to Payment
+                  </button>
                 </div>
-            </form>
+              </form>
+            )}
+          {flag === "payment" && (
+            <>
+              <form className="payment-method">
+                <h2>Payment Method</h2>
+                <div>
+                  <div>
+                    <input type="radio" name="typeOfCard" />
+                    <label forhtml="typeOfCard">Visa</label>
+                  </div>
+                  <div>
+                    <input type="radio" name="typeOfCard" />
+                    <label forhtml="typeOfCard">Mastercard</label>
+                  </div>
+                </div>
+                <div>
+                  <input type="text" name="fName" placeholder="First Name" />
+                  <input type="text" name="lName" placeholder="Last Name" />
+                </div>
+                <input
+                  type="number"
+                  name="cardNumber"
+                  placeholder="Card Number"
+                />
+                <div>
+                  <input
+                    type="number"
+                    name="expiration"
+                    placeholder="Expiration"
+                  />
+                  <input type="number" name="cvv" placeholder="CVV" />
+                </div>
+              </form>
+              <form className="billing-address">
+                <h2>Billing Address</h2>
+                <div className="first-option">
+                  <input type="radio" name="bAddress" />
+                  <label htmlFor="bAddress">Same as shipping address</label>
+                </div>
+                <div>
+                  <input type="radio" name="bAddress" />
+                  <label forhtml="bAddress">Different Address</label>
+                </div>
+                
+                <div className="billing-btns">
+                  <button className="return-to-shipping-btn" onClick={handleReturnToShipping}>Return to Shipping</button>
+                  <button className="pay-now-btn">Pay Now</button>
+                </div>
+              </form>
+            </>
           )}
         </div>
         <div className="right-section">
@@ -120,10 +224,15 @@ export const Checkout = () => {
               <h3>Contact Information</h3>
               <p>John doe Email</p>
             </div>
-            {shipping !== initialShipping && (
+            {(flag === "shipping-method" || flag !== "shipping") && (
               <div className="shipping-information">
                 <h3>Shipping Information</h3>
                 <p>{shipping.address}</p>
+              </div>
+            )}
+            {flag === "payment" && (
+              <div className="shipping-method-info">
+                <h3>Shipping Method</h3>
               </div>
             )}
           </div>
